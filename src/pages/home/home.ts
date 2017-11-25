5053d39210cef9e656d1f2247f72a8440422903b
Angel Hita <angelita@MacBook-Air-de-Angel.local>
2017-11-25 17:55:09 +0100

    first version of bagoaz in ionic 2

---
+++ src/pages/home/home.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { NavController} from 'ionic-angular';
+import { Http } from '@angular/http';
+import { LoadingController } from 'ionic-angular';
+import { readGaia } from '../readGaia/readGaia';
+import 'rxjs/add/operator/map';
+
+// import { DatabaseService } from '../../app/services/databaseService';
+
+let allData;
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+
+export class HomePage {
+  dataBase:object;
+  gaiak:Array<Object>;
+
+  // getDatabase(){
+  //   return this.dataBase;
+  // }
+
+  constructor(public navCtrl: NavController,
+    public http: Http,
+    public loadingCtrl: LoadingController,
+  ) {
+    let loader = this.loadingCtrl.create({
+      content:'Cargando...'
+    });
+    loader.present();
+    this.navCtrl = navCtrl;
+    http.get("https://raw.githubusercontent.com/litospayaso/bagoaz-ionic/master/www/database/bagoaz-export.json").map(res => res.json()).subscribe(response => {
+      this.dataBase = response;
+      allData = response;
+      console.info(this.dataBase);
+      this.gaiak = response.gaiak;
+      loader.dismiss();
+    },err => {
+      loader.dismiss();
+    });
+
+  }
+  clickGaia(e){
+    this.navCtrl.push(readGaia,{gaia:e});
+  }
+  ngOnInit(){
+    // this.databaseService.getDatabase();
+  }
+}
+
+export let database = ()=>{return allData};
---
+++ src/pages/readGaia/readGaia.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams} from 'ionic-angular';
+import { Http } from '@angular/http';
+import { LoadingController } from 'ionic-angular';
+import { database } from '../home/home';
+import 'rxjs/add/operator/map';
+
+// import { DatabaseService } from '../../app/services/databaseService';
+
+@Component({
+  selector: 'readGaia',
+  templateUrl: 'readGaia.html'
+})
+export class readGaia {
+  gaia:string;
+  izenburu:string;
+
+  constructor(public navCtrl: NavController,
+    public http: Http,
+    public loadingCtrl: LoadingController,
+    public navParams: NavParams
+  ) {
+    let lesson = navParams.get('gaia')-1
+    this.izenburu = database().gaiak[lesson].izenburu;
+    this.gaia = database().gaiak[lesson].gaia;
+  }
+  ngOnInit(){
+    console.info("gaia",this.gaia);
+    console.info("home",database());
+  }
+}
