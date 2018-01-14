1ebfdc044aeb486e05e3c213f5e2628f3d2f0f03
Angel Hita <angelita@MacBook-Air-de-Angel.local>
2018-01-14 22:15:55 +0100

    adding button go to activities

--- src/pages/readGaia/readGaia.ts
+++ src/pages/readGaia/readGaia.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams} from 'ionic-angular';
 import { database } from '../home/home';
+import { evalAriketak } from '../evalAriketak/evalAriketak';
 import 'rxjs/add/operator/map';
 
 // import { DatabaseService } from '../../app/services/databaseService';
@@ -10,7 +11,8 @@ import 'rxjs/add/operator/map';
   templateUrl: 'readGaia.html'
 })
 export class readGaia {
-  gaia:string;
+  gaia:object;
+  currentGaia:string;
   izenburu:string;
 
   constructor(public navCtrl: NavController,
@@ -18,11 +20,15 @@ export class readGaia {
   ) {
     let lesson = navParams.get('gaia')-1
     this.izenburu = database().gaiak[lesson].izenburu;
+    this.gaia = database().gaiak[lesson];
     let currentGaia = database().gaiak[lesson].gaia;
     let hizta = navParams.get('hizta');
     if(hizta){
       currentGaia = currentGaia.replace(new RegExp(hizta, 'gi'),"<span class='highlight'>"+hizta+"</span>")
     }
-    this.gaia = currentGaia;
+    this.currentGaia = currentGaia;
+  }
+  clickAriketak(e){
+    this.navCtrl.push(evalAriketak,{gaia:e});
   }
 }
