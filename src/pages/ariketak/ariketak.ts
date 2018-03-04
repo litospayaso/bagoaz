e433aadb02d26e8b460f9a8bd41672efb0bf165c
Angel Hita <angelita@MacBook-Air-de-Angel.local>
2018-03-04 18:27:11 +0100

    adding cards styles to home

--- src/pages/ariketak/ariketak.ts
+++ src/pages/ariketak/ariketak.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController} from 'ionic-angular';
 import { database } from '../home/home';
 import { evalAriketak } from '../evalAriketak/evalAriketak';
+import { Storage } from '@ionic/storage';
 import 'rxjs/add/operator/map';
 
 
@@ -12,11 +13,25 @@ import 'rxjs/add/operator/map';
 
 export class ariketak {
   gaiak:Array<Object>;
-  constructor(public navCtrl: NavController
+  lessonPassed: number[] = [];
+
+  constructor(public navCtrl: NavController,
+    public storage: Storage
   ) {
     this.gaiak = database().gaiak;
+    this.storage.get('lessonPassed').then(val => {
+      if (val) {
+        this.lessonPassed = JSON.parse(val);
+      }
+    });
   }
   clickAriketak(e){
     this.navCtrl.push(evalAriketak,{gaia:e});
+  }
+  completeClass(e) {
+    if (this.lessonPassed.includes(e)) {
+      return "passed"
+    }
+    return "none"
   }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController} from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 import { Http } from '@angular/http';
 import { LoadingController } from 'ionic-angular';
 import { readGaia } from '../readGaia/readGaia';
@@ -15,16 +15,22 @@ let allData;
 })
 
 export class HomePage {
-  dataBase:object;
-  gaiak:Array<Object>;
+  dataBase: object;
+  gaiak: Array<Object>;
+  lessonPassed: number[] = [];
 
   constructor(public navCtrl: NavController,
     public http: Http,
     public loadingCtrl: LoadingController,
-    public storage : Storage
+    public storage: Storage
   ) {
+    this.storage.get('lessonPassed').then(val => {
+      if (val) {
+        this.lessonPassed = JSON.parse(val);
+      }
+    });
     let loader = this.loadingCtrl.create({
-      content:'Cargando...'
+      content: 'Cargando...'
     });
     loader.present();
     this.navCtrl = navCtrl;
@@ -32,51 +38,47 @@ export class HomePage {
     http.get("https://raw.githubusercontent.com/litospayaso/bagoaz-ionic/master/www/database/bagoaz-export.json").map(res => res.json()).subscribe(response => {
       this.dataBase = response;
       allData = response;
-      this.storage.set('allData',JSON.stringify(allData));
+      this.storage.set('allData', JSON.stringify(allData));
       console.info(this.dataBase);
       this.gaiak = response.gaiak;
       loader.dismiss();
       // this.navCtrl.push(evalAriketak,{gaia:36});
-    },err => {
+    }, err => {
       storage.get('allData').then((val) => {
-        if(val){
+        if (val) {
           let response = JSON.parse(val);
           this.dataBase = response;
           allData = response;
-          console.info("you don't have internet conection, running with cache.",this.dataBase);
+          console.info("you don't have internet conection, running with cache.", this.dataBase);
           this.gaiak = response.gaiak;
           loader.dismiss();
           // this.navCtrl.push(evalAriketak,{gaia:36});
           // this.navCtrl.push(readGaia,{gaia:17});
-        }else{
+        } else {
           http.get("../../assets/database/bagoaz-export.json").map(res => res.json()).subscribe(response => {
             this.dataBase = response;
             allData = response;
-            console.info("you don't have internet conection, running with local json.",this.dataBase);
+            console.info("you don't have internet conection, running with local json.", this.dataBase);
             this.gaiak = response.gaiak;
             loader.dismiss();
-          },err => {
+          }, err => {
             this.dataBase = undefined;
             loader.dismiss();
-          });    
+          });
         }
       });
     });
 
   }
-  clickGaia(e){
-    this.navCtrl.push(readGaia,{gaia:e});
+  clickGaia(e) {
+    this.navCtrl.push(readGaia, { gaia: e });
   }
-  completeClass(e){
-    this.storage.get('lessonPassed').then(val => {
-      if(val){
-        let lessonPassed = JSON.parse(val);
-        if(lessonPassed.includes(e)){
-          return "passed"
-        }
-      }
-    });
+  completeClass(e) {
+    if (this.lessonPassed.includes(e)) {
+      return "passed"
+    }
+    return "none"
   }
 }
 
-export const database = ()=>{return allData};
+export const database = () => { return allData };
